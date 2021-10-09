3bed517d883a945be6ea48496262e580cad04e27
unknown <sankalp.umr@gmail.com>
2021-10-09 20:29:28 +0530

    Scanner class method added

--- src/main/java/com/bridglabz/bookstoremangementsystem/utility/UtilScanner.java
+++ src/main/java/com/bridglabz/bookstoremangementsystem/utility/UtilScanner.java
@@ -1,5 +1,27 @@
 package com.bridglabz.bookstoremangementsystem.utility;
 
+import java.util.Scanner;
+
 public class UtilScanner {
 
+	private static final Scanner SCANNER = new Scanner(System.in);
+
+	public static int getInt(String message) {
+		message(message);
+		return SCANNER.nextInt();
+	}
+
+	public static String getString(String message) {
+		message(message);
+		return SCANNER.next();
+	}
+
+	public static double getDouble(String message) {
+		message(message);
+		return SCANNER.nextDouble();
+	}
+
+	private static void message(String msg) {
+		System.out.println("Enter the " + msg);
+	}
 }
