9980d7a596c437eb6958b37a9bc177d9cfe85fb0
ashvini deokate <ashvinideokate3@gmail.com>
2021-10-09 21:03:51 +0530

    Book methods

--- src/main/java/com/bridglabz/bookstoremangementsystem/interfaces/IBookService.java
+++ src/main/java/com/bridglabz/bookstoremangementsystem/interfaces/IBookService.java
@@ -1,4 +1,14 @@
 package com.bridglabz.bookstoremangementsystem.interfaces;
 
+import com.bridglabz.bookstoremangementsystem.entity.Book;
+
 public interface IBookService {
+	
+	public void addBook(Book book);
+	
+	public void deleteBookbyId(Book book,String id);
+	
+	public void searchBookbyId(String id);
+	
+	public void updateBookbyId();
 }
--- src/main/java/com/bridglabz/bookstoremangementsystem/services/BookServiceImpl.java
+++ src/main/java/com/bridglabz/bookstoremangementsystem/services/BookServiceImpl.java
@@ -1,5 +1,32 @@
 package com.bridglabz.bookstoremangementsystem.services;
 
-public class BookServiceImpl {
+import com.bridglabz.bookstoremangementsystem.entity.Book;
+import com.bridglabz.bookstoremangementsystem.interfaces.IBookService;
+
+public class BookServiceImpl implements IBookService {
+
+	@Override
+	public void addBook(Book book) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteBookbyId(Book book, String id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void searchBookbyId(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBookbyId() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
