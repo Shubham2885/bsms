d1087151f15ed2072ae3a25623e7325f6cff8f13
Dinesh Kumar <mr.dineshdkd@gmail.com>
2021-10-09 20:18:46 +0530

    Book Properties Have Been Declared

--- src/main/java/com/bridglabz/bookstoremangementsystem/entity/Book.java
+++ src/main/java/com/bridglabz/bookstoremangementsystem/entity/Book.java
@@ -1,4 +1,10 @@
 package com.bridglabz.bookstoremangementsystem.entity;
 
 public class Book {
+	String bookName;
+	String bookId;
+	String bookAuthor;
+	String bookType;
+	String bookPrice;
+	String bookDesc;
 }
