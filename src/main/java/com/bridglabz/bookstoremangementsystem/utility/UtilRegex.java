b8c684a04ac0b9c772804b0a32933d428ffeec8b
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-10-09 20:37:43 +0530

    Shaikh Afzal: Created UtilRegex & UtilValidation Class And Added Impl.

---
+++ src/main/java/com/bridglabz/bookstoremangementsystem/utility/UtilRegex.java
@@ -0,0 +1,9 @@
+package com.bridglabz.bookstoremangementsystem.utility;
+
+public class UtilRegex {
+	public static final String ID = "^[0-9]{3}$";
+	public static final String NAME = "^[A-Z]{1}[A-Za-z]{1,9}$";
+	public static final String MOBILE = "^[6-9]{1}[1-9]{9}$";
+	public static final String EMAIL = "^[A-Za-z0-9+_.-]+@(.+)$";
+	public static final String ADDRESS = "^[A-Z]{1}[A-Za-z]{1,20}$";
+}
