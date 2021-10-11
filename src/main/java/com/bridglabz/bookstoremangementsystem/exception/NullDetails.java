f32635c3cc9318c92b0b48e428442dbea39e2967
ashvini deokate <ashvinideokate3@gmail.com>
2021-10-11 20:14:52 +0530

    Custome Exception Added

---
+++ src/main/java/com/bridglabz/bookstoremangementsystem/exception/NullDetails.java
@@ -0,0 +1,9 @@
+package com.bridglabz.bookstoremangementsystem.exception;
+
+public class NullDetails extends Exception {
+
+//Author : Ashvini	
+	public NullDetails(String msg){
+	super(msg);
+}
+}
