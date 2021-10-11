e121a46de822b76194906ccc8b2630ac81e129af
manoj gaikwad <gaikwadm853@gmail.com>
2021-10-11 20:26:43 +0530

    Added: manoj: addUser in UserController and pass data addUser in UserServiceImpl

--- src/main/java/com/bridglabz/bookstoremangementsystem/interfaces/IUserService.java
+++ src/main/java/com/bridglabz/bookstoremangementsystem/interfaces/IUserService.java
@@ -1,7 +1,10 @@
 package com.bridglabz.bookstoremangementsystem.interfaces;
 
+import com.bridglabz.bookstoremangementsystem.entity.User;
+import com.bridglabz.bookstoremangementsystem.exception.NullDetails;
+
 public interface IUserService {
-	void add();
+	void add(User user) throws NullDetails;
 	void deleteById();
 	void searchById();
 	void updateById();
