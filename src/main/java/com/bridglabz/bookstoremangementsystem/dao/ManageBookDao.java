81322279bafb37033de41970eb1ead925e696dba
unknown <sankalp.umr@gmail.com>
2021-10-11 20:23:06 +0530

    added book manage dao class

---
+++ src/main/java/com/bridglabz/bookstoremangementsystem/dao/ManageBookDao.java
@@ -0,0 +1,40 @@
+package com.bridglabz.bookstoremangementsystem.dao;
+
+import java.util.List;
+
+import com.bridglabz.bookstoremangementsystem.entity.ManageBook;
+import com.bridglabz.bookstoremangementsystem.interfaces.IManageBook;
+
+public class ManageBookDao implements IManageBook {
+
+	@Override
+	public void addManageBook(ManageBook manageBook) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ManageBook deleteById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ManageBook searchById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ManageBook updateById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ManageBook> getAllManageBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
